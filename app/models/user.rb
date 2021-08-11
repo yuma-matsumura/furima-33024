8bb91b66646ee8761cf7a9a4cfb428737c964e27
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-11 16:07:33 +0900

    syntaxエラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates: nickname, presence: true
-         validates: email, uniqueness: true
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-         validates: password, format: { with: VALID_PASSWORD_REGEX}
+  # validates :nickname, presence: true
+  validates :email, uniqueness: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
 
-         
-         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/i.freeze
-         validates: family_name, presence: true ,format: {with: VALID_NAME_REGEX}
-         validates: first_name, presence: true ,format: {with: VALID_NAME_REGEX}
-         
-         VALID_NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/i.freeze
-         validates: family_name_kana, presence: true ,format: {VALID_NAME_KANA_REGEX}
-         validates: first_name_kana, presence: true ,format: {VALID_NAME_KANA_REGEX}
-         validates: birthday, presence: true 
 
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/i.freeze
+  with_options presence: true,format: {with: VALID_NAME_REGEX} do
+    validates :family_name
+    validates :first_name
+  end
+
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/i.freeze
+  with_options presence: true,format: {with: VALID_NAME_KANA_REGEX} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 end
 
