855f77f6feb4e4041f605a84aa412a43955f4189
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-12 21:37:59 +0900

    Rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,20 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  with_options format: {with: VALID_PASSWORD_REGEX} do
+  with_options format: { with: VALID_PASSWORD_REGEX } do
     validates :password
   end
 
-
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/i.freeze
-  with_options presence: true,format: {with: VALID_NAME_REGEX} do
+  with_options presence: true, format: { with: VALID_NAME_REGEX } do
     validates :family_name
     validates :first_name
   end
 
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/i.freeze
-  with_options presence: true,format: {with: VALID_NAME_KANA_REGEX} do
+  with_options presence: true, format: { with: VALID_NAME_KANA_REGEX } do
     validates :family_name_kana
     validates :first_name_kana
   end
   validates :birthday, presence: true
-
 end
-
