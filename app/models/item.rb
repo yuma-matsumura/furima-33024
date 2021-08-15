c3f98784bdeeabc0ff13f94bd3e0636da9577f92
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-15 21:56:11 +0900

    ActiveHash導入とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,35 @@
 class Item < ApplicationRecord
-  validates :product_name, presence: true
-  validates :text, presence: true
-  validates :category_id, presence: true
-  validates :password_confirmation, presence: true
-  validates :shipping_charge, presence: true
-  validates :shipping_area, presence: true
-  validates :delivery_time, presence: true
-  validates :price, presence: true
+
+  VALID_PRICE_REGEX = /\A[0-9]+\z/i.freeze
+  with_options format: { with: VALID_PRICE_REGEX } do
+    validates :price
+  end
+
+  with_options presence: true do
+    validates :product_name
+    validates :text
+    validates :price, inclusion: { in: 300..9_999_999 }
+  end
+
+  validates :price, numericality: true
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :password_confirmation_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :delivery_time_id
+  end
+
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :delivery_time
 
 end
