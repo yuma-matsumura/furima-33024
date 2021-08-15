286dd51ed2aae553025b2ea5f67a2b3a4e72ed01
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-15 18:01:21 +0900

    Active Storageの導入2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,3 +2,8 @@ class ItemsController < ApplicationController
   def index
   end
 end
+
+private
+
+def item_params
+  params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,6 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   belongs_to :user
+  has_one_attached :image
 
 end
