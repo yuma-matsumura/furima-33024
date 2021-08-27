620754452055b4abffcdfc000db1998819ed5b46
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-27 19:37:17 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show]
-
+  before_action :set_item, only: [:show, :edit]
+  before_action :authenticate_user!, only: [:edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,6 +21,19 @@ class ItemsController < ApplicationController
 
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+     @item = Item.find(params[:id])
+    @item.update(item_params)
+     if @item.save
+      redirect_to item_path
+     else
+      render :edit
+     end
   end
 end
 
