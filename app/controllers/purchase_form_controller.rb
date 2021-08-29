c28133d3007bdfdb470afd5f415d47d8527a2fe2
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-29 21:47:14 +0900

    PurchaseFormControllerへの、カラム情報保存の記述

--- app/controllers/purchase_form_controller.rb
+++ app/controllers/purchase_form_controller.rb
@@ -1,2 +1,17 @@
 class PurchaseFormController < ApplicationController
+  def index
+  end
+
+  def create
+    @purchase = PurchaseForm.new(set_params)
+    if @purchase.valid?
+       @purchase.save
+    end
+
+  end
+
+  private
+  def set_params
+    params.permit(:user_id, :item_id, :order_id, :postal_code, :shipping_area_id, :city, :house_number, :building_name, :telephone_number)
+  end
 end
