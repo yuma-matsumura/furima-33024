0017a3c4259de68c2e47472a238d2ceaea329097
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-29 22:01:31 +0900

    アソシエーションの記述

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -11,6 +11,7 @@ class PurchaseForm
     validates :telephone_number
   end
 
+
 def save
   Order.create(user_id: user_id, item_id: item_id)
   Address.create(order_id: order_id, postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number)
