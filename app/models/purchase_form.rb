7f6d0bedf3700307e3275ec0f8e63acd7094b466
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-29 20:11:05 +0900

    モデルの作成

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,2 @@
+class PurchaseForm < ApplicationRecord
+end
---
+++ db/migrate/20210829110903_create_purchase_forms.rb
@@ -0,0 +1,8 @@
+class CreatePurchaseForms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchase_forms do |t|
+
+      t.timestamps
+    end
+  end
+end
