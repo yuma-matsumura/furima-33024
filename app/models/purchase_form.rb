fa78fc88a11586ae22f633a5451baa6513563649
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-30 21:55:10 +0900

    単体テストコード1

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -7,16 +7,27 @@ class PurchaseForm
     validates :shipping_area_id
     validates :city
     validates :house_number
-    validates :building_name
     validates :telephone_number
   end
 
-  VALID_CITY_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :shipping_area_id
+  end
 
-def save
+  VALID_CITY_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  with_options format: { with: VALID_CITY_REGEX } do
+    validates :postal_code
+  end
+
+  VALID_TELEPHONE_REGEX = /\A\d{,11}\z/.freeze
+  with_options format: { with: VALID_TELEPHONE_REGEX } do
+    validates :telephone_number
+  end
+
+
+
+  def save
   Order.create(user_id: user_id, item_id: item_id)
   Address.create(order_id: order_id, postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number)
-
-
-end
+  end
 end
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -1,5 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseForm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase_form)
+  end
+
+  describe '商品の購入' do
+    context '商品購入がうまくいく場合' do
+      it 'FactoryBotが機能している場合' do
+        expect(@purchase).to be_valid
+      end
+    end
+
+    context '商品の購入がうまくできない場合' do
+      it '郵便番号が空では保存できない' do
+        @purchase.postal_code = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '都道府県が空では保存できない' do
+        @purchase.shipping_area_id = 1
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Shipping area can't be blank")
+      end
+      it '市区町村が空では保存できない' do
+      @purchase.city = ''
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空では保存できない' do
+      @purchase.house_number = ''
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号が空では保存できない' do
+      @purchase.telephone_number = ''
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Telephone number can't be blank")
+      end
+
+      it '郵便番号に-がない場合保存できない' do
+      @purchase.postal_code = '3334444'
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Postal code is invalid")
+      end
+
+      it '電話番号が12桁の場合保存できない' do
+      @purchase.telephone_number = '111111111111'
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Telephone number is invalid")
+      end
+
+      it '電話番号に-がある場合保存できない' do
+      @purchase.telephone_number ='1-1'
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Telephone number is invalid")
+      end
+
+
+
+
+
+
+
+
+
+
+
+
+    end
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
