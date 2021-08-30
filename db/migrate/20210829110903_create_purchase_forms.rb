25cd28ef6708ef9f56ad0e35e38c9abb3fc868d5
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-08-30 15:01:50 +0900

    マイグレーションファイルへの記述

--- db/migrate/20210829110903_create_purchase_forms.rb
+++
@@ -1,8 +0,0 @@
-class CreatePurchaseForms < ActiveRecord::Migration[6.0]
-  def change
-    create_table :purchase_forms do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210829111046_create_addresses.rb
+++ db/migrate/20210829111046_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.references :order, foreign_key: true
+      t.string :postal_code, null: false
+      t.integer :shipping_area_id, null: false
+      t.string :city, null: false
+      t.string :house_number, null: false
+      t.string :building_name, null: false
+      t.string :telephone_number, null: false
       t.timestamps
     end
   end
