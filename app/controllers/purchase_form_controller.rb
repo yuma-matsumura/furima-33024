b1fa2b1237c36e20f6b3a223f56e1371852832cb
yuma-matsumura <y_sp1_der_212@icloud.com>
2021-09-01 19:50:25 +0900

    URLを直接入力した際の処理

--- app/controllers/purchase_form_controller.rb
+++ app/controllers/purchase_form_controller.rb
@@ -1,6 +1,8 @@
 class PurchaseFormController < ApplicationController
   before_action :set_purchase, only:[:index, :create]
   before_action :move_to_signed_in, only:[:index, :create]
+  before_action :move_to_top_page, only:[:index]
+  before_action :move_to_top_page_sold_out, only:[:index]
   def index
     @purchase = PurchaseForm.new
   end
@@ -17,6 +19,11 @@ class PurchaseFormController < ApplicationController
   end
 
   private
+  def move_to_top_page_sold_out
+    if @item.order.present?
+      redirect_to root_path
+    end
+  end
   def set_params
     params.require(:purchase_form).permit(:postal_code, :shipping_area_id, :city, :house_number, :building_name, :telephone_number).merge(token: params[:token],item_id: params[:item_id],user_id: current_user.id)
   end
@@ -26,8 +33,11 @@ class PurchaseFormController < ApplicationController
   def move_to_signed_in
     unless user_signed_in?
       redirect_to root_path
+    end
   end
-end
+  def move_to_top_page
+    redirect_to root_path if current_user == @item.user
+  end
   def move_to_top
     if current_user == @item.user
       redirect_to root_path
